20f2827b8b7b0edce63c30f96568522514e914e2
rabee361 <rha60540@gmail.com>
2024-06-22 15:14:08 +0300

    changes

--- project/urls.py
+++ project/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('' , include('accounts.urls')),
-    path('charts/' , include('charts.urls'))
+    path('project/admin/', admin.site.urls),
+    path('project/' , include('accounts.urls')),
+    path('project/charts/' , include('charts.urls'))
 
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
 
