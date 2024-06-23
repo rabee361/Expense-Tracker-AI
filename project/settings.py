d6ddcf02b771e91c628a6d35edc0547249074185
rabee361 <rha60540@gmail.com>
2024-06-23 23:44:07 +0300

    changes

--- project/settings.py
+++ project/settings.py
@@ -150,9 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-STATIC_URL = '/home/app/static/'
+STATIC_URL = '/project/static/'
 
-MEDIA_URL = '/home/app/media/'
+MEDIA_URL = '/project/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 # Default primary key field type
