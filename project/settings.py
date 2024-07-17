da0e61201c500b7316eb01129e9489107fa52429
rabee361 <rha60540@gmail.com>
2024-07-17 19:36:32 +0300

    changes

--- project/settings.py
+++ project/settings.py
@@ -79,22 +79,22 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:9000',
-    'http://85.31.237.33:9000'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:9000',
+#     'http://85.31.237.33:9000'
+# ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://85.31.237.33:9000',
-    'http://localhost:9000'
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://85.31.237.33:9000',
+#     'http://localhost:9000'
+# ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://85.31.237.33',
-    'http://localhost'
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://85.31.237.33',
+#     'http://localhost'
+# ]
 
 ROOT_URLCONF = 'project.urls'
 
