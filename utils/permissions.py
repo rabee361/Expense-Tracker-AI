cd9330651556727b9d5cd79c5c39c5ca24c8407b
rabee361 <rha60540@gmail.com>
2024-08-03 17:49:25 +0300

    upcoming payments

--- utils/permissions.py
+++ utils/permissions.py
@@ -19,8 +19,11 @@ class IsVerified(BasePermission):
 
 class HaveOTPCode(BasePermission):
     def has_permission(self, request, view):
-        user_id = request.pk
-        code_verification = OTPCode.objects.filter(user__id=user_id).first()
-        if not code_verification:
-            raise PermissionDenied("الرجاء طلب رمز التحقق والمحاولة من جديد")
-        return True
+        user_id = view.kwargs.get('pk', None)
+        if pk is not None:
+            code_verification = OTPCode.objects.filter(user__id=user_id).first()
+            if not code_verification:
+                raise PermissionDenied("الرجاء طلب رمز التحقق والمحاولة من جديد")
+            return True
+        else:
+            return False
