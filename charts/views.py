6f3ae0d33ee030b434634f9b61912147775378bb
rabee361 <rha60540@gmail.com>
2024-08-10 22:09:39 +0300

    my items

--- charts/views.py
+++ charts/views.py
@@ -13,7 +13,7 @@ from rest_framework import status
 from django.db import IntegrityError
 
 
-class CreateItemView(CreateAPIView):
+class CreateItemView(ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     filter_backends = [DjangoFilterBackend]
