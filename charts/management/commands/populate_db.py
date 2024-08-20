f413ab0faa9deb43b6120b875a7f916f6794a1d2
rabee361 <rha60540@gmail.com>
2024-08-20 03:55:19 +0300

    populate

--- charts/management/commands/populate_db.py
+++ charts/management/commands/populate_db.py
@@ -14,13 +14,13 @@ class Command(BaseCommand):
         
         amount = options["amount"] if options["amount"] else 500
 
-        account = Account.objects.get(id=1)
+        account = Account.objects.get(id=40)
         for i in range(0,amount):
             dt = pytz.utc.localize(datetime.now() - timedelta(days = random.randint(0 , 1825)))
             expense = Item.objects.create(
                 account = account,
                 item_name = str(f'item {i}'),
-                price = random.randrange(500,50000,50),
+                price = i * 20,
                 subcategory = random.choice(types)
             )
             expense.created = dt
